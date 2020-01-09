b1b12ab0cadc1a88a068d59b970eaf782ed8cbbb
Sithija Shehara <nsithijashehara@gmail.com>
2020-01-09 20:00:26 +0530

    initial stage of SM login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import Auth from "./Auth";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        marginTop:250,
+        marginLeft:'50',
+        alignItems:'center',
+        justifyContent:"center",
+        height:500,
+        width:500,
+        backgroundColor:'#f1f2f6'
+      }}
+    >
+      <Auth />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Auth.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./App.css";
+
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAjSRy3TtXjpON7pO1F7MYhVxb6m644qEw",
+  authDomain: "patron-b11e8.firebaseapp.com"
+});
+
+class Auth extends React.Component {
+  state = { isSignedIn: false };
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  };
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user });
+    });
+  };
+  render() {
+    return (
+      <div className="mainContainer">
+        {this.state.isSignedIn ? (
+          <div>
+            <h3>Sign in</h3>
+            <button onClick={()=> firebase.auth().signOut()}>Sign out</button>
+          </div>
+        ) : (
+          <div  style={{display: 'flex', justifyContent: 'center'}}>
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+export default Auth;
